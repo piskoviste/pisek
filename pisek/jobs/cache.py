5f553cb107f16526a746b234ad099b2dab74ea2d
Daniel Skýpala <daniel@honza.info>
2025-04-07 11:39:46 +0200

    Cache: Swap cache file when completed

--- pisek/jobs/cache.py
+++ pisek/jobs/cache.py
@@ -136,6 +136,7 @@ class Cache:
 
     def export(self) -> None:
         """Export cache into a file."""
-        with open(CACHE_CONTENT_FILE, "wb") as f:
+        with open(CACHE_CONTENT_FILE + ".tmp", "wb") as f:
             pickle.dump(self.cache, f)
+        os.rename(CACHE_CONTENT_FILE + ".tmp", CACHE_CONTENT_FILE)
         self.last_save = time.time()
