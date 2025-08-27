972152d1abb20da22e2e2bb80e90eadac9c049cb
Daniel Skýpala <daniel@honza.info>
2025-08-27 17:59:45 +0200

    fix(job-pipeline): Handle managers failing during job creation

--- pisek/jobs/jobs.py
+++ pisek/jobs/jobs.py
@@ -405,7 +405,6 @@ class JobManager(PipelineItem):
 
     def finalize(self) -> None:
         """Does final evaluation and computes the result of this job manager."""
-        assert self.state == State.running
         if not self.any_failed():
             try:
                 self._evaluate()
