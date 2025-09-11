79019d546a7aaf7b86bb6ffed652f523904339f9
Daniel Skýpala <daniel@honza.info>
2025-09-11 09:47:47 +0200

    feat(utils): Add method to color background

--- pisek/utils/colors.py
+++ pisek/utils/colors.py
@@ -1,4 +1,4 @@
-from colorama import Fore
+from colorama import Fore, Back
 
 
 class __ColorSettings:
@@ -11,14 +11,20 @@ class __ColorSettings:
         """Sets whether colors should be displayed."""
         self.colors_on = colors_on
 
-    def colored(self, msg: str, color: str) -> str:
+    def _colored(self, what, msg: str, color: str) -> str:
         """Recolors all white text to given color."""
         if not self.colors_on:
             return msg
 
-        col = getattr(Fore, color.upper())
-        msg = msg.replace(f"{Fore.RESET}", f"{Fore.RESET}{col}")
-        return f"{col}{msg}{Fore.RESET}"
+        col = getattr(what, color.upper())
+        msg = msg.replace(f"{what.RESET}", f"{what.RESET}{col}")
+        return f"{col}{msg}{what.RESET}"
+
+    def colored(self, msg: str, color: str) -> str:
+        return self._colored(Fore, msg, color)
+
+    def colored_back(self, msg: str, color: str) -> str:
+        return self._colored(Back, msg, color)
 
 
 ColorSettings = __ColorSettings()
