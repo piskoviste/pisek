ebba28dc697d29579a10fbc5ae2e8f528b626e0c
Daniel Skýpala <skipy@kam.mff.cuni.cz>
2026-01-23 15:10:36 +0100

    test(max): Test go

---
+++ fixtures/max/solve_go_complex/solve.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Fscan(in, &n)
+	nums := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &nums[i])
+	}
+	fmt.Println(max(nums))
+}
